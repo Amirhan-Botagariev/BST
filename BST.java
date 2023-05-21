d91b6fc80858eaddbe7bdfb76a65c221567fc06c
Amirhan-Botagariev <amirkhan1botagariev@mail.ru>
2023-05-21 23:48:33 +0600

    Added delete, deleteNode and findMin methods

--- BST.java
+++ BST.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BST <K extends Comparable<K>, V>{
     private Node root;
     private class Node{
@@ -59,4 +61,40 @@ public class BST <K extends Comparable<K>, V>{
         }
     }
 
+    public void delete(K key){
+        this.root = deleteNode(root, key);
+        size--;
+    }
+
+    private Node deleteNode(Node root, K key) {
+        if (root == null){
+            return null;
+        }
+        if (key.compareTo(root.key) == 1){
+            root.left = deleteNode(root.left, key);
+        } else if (key.compareTo(root.key) == -1){
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null && root.right == null){
+                return null;
+            } else if (root.left == null){
+                return root.right;
+            } else if (root.right == null){
+                return root.left;
+            } else {
+                Node min = findMin(root);
+                root.val = min.val;
+                root.key = min.key;
+                root.right = deleteNode(root.right, min.key);
+            }
+        }
+        return root;
+    }
+
+    private Node findMin(Node root) {
+        while (root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
 }
