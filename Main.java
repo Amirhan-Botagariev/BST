6bd002b29bd1c29928bbd4229916e94c77aaa0f6
Amirhan-Botagariev <amirkhan1botagariev@mail.ru>
2023-05-22 00:09:11 +0600

    Tested BST class

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        BST<Integer, Integer> bst = new BST<>();
+        for (int i = 0; i < 10; i++){
+            bst.put(i, i);
+        }
 
+        Iterable iterable = bst.iterator();
+
+        for (Object el : iterable){
+            System.out.println(el);
+        }
     }
 }
