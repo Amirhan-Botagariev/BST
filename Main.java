bbf657297a60095b8bf37902314f5668753236cd
Amirhan-Botagariev <amirkhan1botagariev@mail.ru>
2023-05-26 09:04:43 +0600

    Tested contains method

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         BST<Integer, Integer> bst = new BST<>();
-        for (int i = 0; i < 10; i++){
+        for (int i = 10; i < 15; i++){
+            bst.put(i, i);
+        }
+        for (int i = 0; i < 5; i++){
             bst.put(i, i);
         }
 
-        Iterable iterable = bst.iterator();
-
-        for (Object el : iterable){
-            System.out.println(el);
-        }
+        System.out.println(bst.contains(3));
     }
 }
